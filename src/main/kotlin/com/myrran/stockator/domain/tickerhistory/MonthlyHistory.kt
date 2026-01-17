9224aeb22c56cc16fbdbcb3a22f05f467c9d945c
Ivan Delgado Huerta <jhanto@gmail.com>
2026-01-17 13:54:27 +0100

    wip

--- src/main/kotlin/com/myrran/stockator/domain/tickerhistory/MonthlyHistory.kt
+++ src/main/kotlin/com/myrran/stockator/domain/tickerhistory/MonthlyHistory.kt
@@ -1,22 +1,44 @@
 package com.myrran.stockator.domain.tickerhistory
 
+import com.myrran.stockator.domain.misc.Increase
 import com.myrran.stockator.domain.misc.Year
+import com.myrran.stockator.domain.misc.average
+import com.myrran.stockator.domain.misc.median
 import java.time.LocalDate
 import java.time.Month
 
 class MonthlyHistory(
     val monthlyRates: List<MonthlyRates>
 ) {
-    val byYearAndMonth: Map<Year, Map<Month, MonthlyRates>> = monthlyRates
+    private val byYearAndMonth: Map<Year, Map<Month, MonthlyRates>> = monthlyRates
         .groupBy { Year(it.closingDay.year) }
         .mapValues { entry -> entry.value.associateBy { it.closingDay.month } }
 
-    val byMonth: Map<Month, List<MonthlyRates>> = monthlyRates
+    private val byMonth: Map<Month, List<MonthlyRates>> = monthlyRates
         .groupBy { it.closingDay.month }
+
+    // MAIN:
+    //--------------------------------------------------------------------------------------------------------
 
     fun firstDate(): LocalDate =
         monthlyRates.minOfOrNull { it.closingDay }!!
 
     fun lastDate(): LocalDate =
         monthlyRates.maxOfOrNull { it.closingDay }!!
+
+    fun averageIncreaseOf(month: Month): Increase =
+        byMonth[month]
+            ?.map { it.increase }
+            ?.average() ?: Increase(0.0)
+
+    fun medianIncreaseOf(month: Month): Increase =
+        byMonth[month]
+            ?.map { it.increase }
+            ?.median() ?: Increase(0.0)
+
+    fun numberOfNegativeIncreasesOn(month: Month): Int =
+        byMonth[month]
+            ?.map { it.increase}
+            ?.filterIsInstance<Increase>()
+            ?.count { it.value < 0 } ?: 0
 }
--- src/main/kotlin/com/myrran/stockator/domain/tickerhistory/TickerHistory.kt
+++ src/main/kotlin/com/myrran/stockator/domain/tickerhistory/TickerHistory.kt
@@ -1,8 +1,6 @@
 package com.myrran.stockator.domain.tickerhistory
 
 import com.myrran.stockator.domain.misc.Increase
-import com.myrran.stockator.domain.misc.average
-import com.myrran.stockator.domain.misc.median
 import java.time.LocalDate
 import java.time.Month
 
@@ -17,11 +15,11 @@ data class TickerHistory(
         monthlyHistory.lastDate()
 
     fun averageIncreaseOf(month: Month): Increase =
-        monthlyHistory.byMonth[month]?.map { it.increase }?.average() ?: Increase(0.0)
+        monthlyHistory.averageIncreaseOf(month)
 
     fun medianIncreaseOf(month: Month): Increase =
-        monthlyHistory.byMonth[month]?.map { it.increase }?.median() ?: Increase(0.0)
+        monthlyHistory.medianIncreaseOf(month)
 
     fun numberOfNegativeIncreasesOn(month: Month): Int =
-        monthlyHistory.byMonth[month]?.map { it.increase}?.filterIsInstance<Increase>()?.count { it.value < 0 } ?: 0
+        monthlyHistory.numberOfNegativeIncreasesOn(month)
 }
